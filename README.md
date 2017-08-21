224092cc0e41b06cc7538d2c4dab41069626d7bc
wk-j <somnuk.wk@gmail.com>
2017-08-22 01:22:09 +0700

    Fun

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## Decorator
+
+- https://github.com/Microsoft/vscode/blob/master/extensions/fsharp/syntaxes/fsharp.json
--- hello.fsx
+++ hello.fsx
@@ -1,9 +1,19 @@
 open System.Linq
 open System
+open System.Threading
+open System.Timers
 
 let x = [100;200;300]
 
-x |> List.map (fun x -> x + 1)
+[1;2;3].Select(fun i -> i + 1) 
 
-[1;2;3].Select(fun i -> i + 1)
+type Record = {
+    X: int
+    Y: int
+}
 
+type Hello = {
+    X: int
+}
+
+let k = (1,2,3,3)
